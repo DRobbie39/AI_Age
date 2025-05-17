904ab04c7e932d089e5e32279638aa4aa2e9a72a
DRobbie39 <phangiadat123@gmail.com>
2025-05-18 01:39:38 +0700

    init home interface

---
+++ AI_Age_FrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AI_Age_FrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
