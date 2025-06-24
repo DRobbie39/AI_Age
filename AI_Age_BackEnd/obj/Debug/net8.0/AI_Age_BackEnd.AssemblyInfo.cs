eb9e731e5484e812ddc05cb66d96ba3532b6d53a
DRobbie39 <phangiadat123@gmail.com>
2025-06-25 06:58:21 +0700

    update navigate chatbox

--- AI_Age_BackEnd/obj/Debug/net8.0/AI_Age_BackEnd.AssemblyInfo.cs
+++ AI_Age_BackEnd/obj/Debug/net8.0/AI_Age_BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Age_BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fcff80348483370b30c5d4564ada340572f65e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee3213024297fcf94c0d5bf8c03196703384ed7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Age_BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Age_BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
