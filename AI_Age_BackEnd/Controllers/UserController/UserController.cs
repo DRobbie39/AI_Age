b03c4a2bb8a5badc9b94c6b7d8c1e06d26104c88
DRobbie39 <phangiadat123@gmail.com>
2025-06-25 23:54:22 +0700

    edit user controller api

--- AI_Age_BackEnd/Controllers/UserController/UserController.cs
+++ AI_Age_BackEnd/Controllers/UserController/UserController.cs
@@ -4,37 +4,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AI_Age_BackEnd.Controllers.UserController
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
-        [ApiController]
-        [Route("api/[controller]")]
         //[Authorize(Roles = "Admin")] // Bật dòng này để chỉ Admin mới có quyền truy cập
-        public class UsersController : ControllerBase
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
         {
-            private readonly UserService _userService;
+            _userService = userService;
+        }
 
-            public UsersController(UserService userService)
-            {
-                _userService = userService;
-            }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        }
 
-            [HttpGet]
-            public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUsers()
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetUserById(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
             {
-                var users = await _userService.GetAllUsersAsync();
-                return Ok(users);
+                return NotFound();
             }
-
-            [HttpGet("{id}")]
-            public async Task<ActionResult<UserDto>> GetUserById(int id)
-            {
-                var user = await _userService.GetUserByIdAsync(id);
-                if (user == null)
-                {
-                    return NotFound();
-                }
-                return Ok(user);
-            }
+            return Ok(user);
         }
     }
 }
--- AI_Age_BackEnd/obj/Debug/net8.0/AI_Age_BackEnd.AssemblyInfo.cs
+++ AI_Age_BackEnd/obj/Debug/net8.0/AI_Age_BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Age_BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2376e3331497f0fb9fd99485785295f910ffba32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e685f99a950d4b2fc1110ea4d7f289580624a0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Age_BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Age_BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
