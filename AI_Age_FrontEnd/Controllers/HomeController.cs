7e684f80e603338bff40b6509359ac3e9ec1831d
DRobbie39 <phangiadat123@gmail.com>
2025-06-27 08:18:35 +0700

    display tool in home

--- AI_Age_FrontEnd/Controllers/HomeController.cs
+++ AI_Age_FrontEnd/Controllers/HomeController.cs
@@ -1,14 +1,40 @@
-using System.Diagnostics;
-using AI_Age_FrontEnd.Models;
+﻿using System.Text.Json;
+using AI_Age_FrontEnd.Models.AIToolViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AI_Age_FrontEnd.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7022/api/");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var viewModel = new HomeViewModel();
+
+            var categoriesResponse = await _httpClient.GetAsync("AIToolCategory");
+            if (categoriesResponse.IsSuccessStatusCode)
+            {
+                var categoriesJson = await categoriesResponse.Content.ReadAsStringAsync();
+                viewModel.Categories = JsonSerializer.Deserialize<List<AIToolCategoryViewModel>>(categoriesJson, jsonOptions) ?? new List<AIToolCategoryViewModel>();
+            }
+
+            var toolsResponse = await _httpClient.GetAsync("AITool");
+            if (toolsResponse.IsSuccessStatusCode)
+            {
+                var toolsJson = await toolsResponse.Content.ReadAsStringAsync();
+                viewModel.AITools = JsonSerializer.Deserialize<List<AIToolViewModel>>(toolsJson, jsonOptions) ?? new List<AIToolViewModel>();
+            }
+
+            return View(viewModel);
         }
     }
 }
---
+++ AI_Age_FrontEnd/Models/AIToolViewModel/AIToolCategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AI_Age_FrontEnd.Models.AIToolViewModel
+{
+    public class AIToolCategoryViewModel
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
