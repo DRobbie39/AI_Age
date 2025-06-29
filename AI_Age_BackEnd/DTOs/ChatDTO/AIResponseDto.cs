f7825d22350c0793ccf92c66ac60e13bf278b1c3
DRobbie39 <phangiadat123@gmail.com>
2025-06-30 01:33:19 +0700

    edit chat service

---
+++ AI_Age_BackEnd/DTOs/ChatDTO/AIResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AI_Age_BackEnd.DTOs.ChatDTO
+{
+    public class AIResponseDto
+    {
+        public string Action { get; set; }
+        public string Response { get; set; }
+        public string? Url { get; set; }
+        public List<SearchResultDto> Suggestions { get; set; } = new List<SearchResultDto>();
+    }
+}
--- AI_Age_BackEnd/obj/Debug/net8.0/AI_Age_BackEnd.AssemblyInfo.cs
+++ AI_Age_BackEnd/obj/Debug/net8.0/AI_Age_BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Age_BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055688587c798fc733aef7b630aa7dab1ae1f3aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282c522642967df5069daa75e666cda2ae7d6542")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Age_BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Age_BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
